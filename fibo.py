bd93053c28826ee90f475ce294652360623f07ef
IDScheelke <IDS@kamstrup.com>
2019-04-01 14:48:29 +0200

    Add addtional print info

--- fibo.py
+++ fibo.py
@@ -3,7 +3,7 @@
 def fib(n):    # write Fibonacci series up to n
     a, b = 0, 1
     while a < n:
-        print("%d " %  (a) )
+        print("value: %d" %  (a) )
         a, b = b, a+b
     print()
 
